7fc37a43f3c667862e10e9b7ea5511c0fae069c9
Darshan bhandary <109609462+darshan45672@users.noreply.github.com>
2023-05-13 14:48:30 +0000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Darshan Bhandary",
+  title: "Web Designer & Software Developer",
+  email: "drshnbhandary@gmail.com",
+  gitHub: "darshan45672",
+  instagram: "mr_dare_striker",
+  linkedIn: "darshan",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
