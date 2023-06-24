f377cbecc6bc80ab796202a61140cbadbce6f095
darshan45672 <drshnbhandary@gmail.com>
2023-06-24 20:05:07 +0530

    edited

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -64,7 +64,7 @@ const Portfolio = () => {
         <div style={{ maxWidth: "40%", alignSelf: "center" }}>
           <img
             src={image}
-            style={{ height: "90%", width: "100%", objectFit: "cover", paddingLeft: "7rem"}}
+            style={{ height: "90%", width: "100%", objectFit: "cover", paddingLeft: "5rem", }}
             alt={imageAltText}
           />
         </div>
